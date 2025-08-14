af2bf32f53780a4b93637355cd4af61701326160
Shahmeer43 <shahmeerhameed59@gmail.com>
2025-08-14 17:09:42 +0500

    file name changed

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const process = require("process");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-const config = require(__dirname + "/../config/db.sequelize.js");
+const config = require(__dirname + "/../config/config.js");
 const db = {};
 
 let sequelize;
