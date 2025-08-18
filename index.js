2cbaba5df6882d789bddd2f898542c22fd4fb3f1
Shahmeer43 <shahmeerhameed59@gmail.com>
2025-08-19 01:04:29 +0500

    set up some files

--- index.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
-const express = require("express");
-
-const app = express();
-
-app.listen(process.env.SERVER_PORT);
---
+++ server.js
@@ -0,0 +1,7 @@
+require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
+
+const app = require("./app");
+
+app.listen(process.env.SERVER_PORT, () =>
+  console.log(`Server running on port ${process.env.SERVER_PORT}`)
+);
