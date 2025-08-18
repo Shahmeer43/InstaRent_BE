5617bffaec57f4abece6567bb3396cc4572c5ab9
Shahmeer43 <shahmeerhameed59@gmail.com>
2025-08-19 00:47:19 +0500

    added user table migration scripts

--- config/config.js
+++ config/config.js
@@ -1,10 +1,27 @@
 // this config file is for sequelize library
+require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
 const dbConfig = {
-  dialect: "postgres",
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
+  development: {
+    dialect: "postgres",
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+  },
+  staging: {
+    dialect: "postgres",
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+  },
+  production: {
+    dialect: "postgres",
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+  },
 };
 
 module.exports = dbConfig;
--- models/index.js
+++ models/index.js
@@ -2,10 +2,11 @@
 
 const fs = require("fs");
 const path = require("path");
-const process = require("process");
 const Sequelize = require("sequelize");
+const process = require("process");
 const basename = path.basename(__filename);
-const config = require(__dirname + "/../config/config.js");
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
 let sequelize;
