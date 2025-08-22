444507f5e6c70b7c16e436d5b76aaca3d508a32d
Shahmeer <shahmeerhameed59@gmail.com>
2025-08-23 04:10:59 +0500

    user model work in progress

---
+++ models/User.js
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "users", // explicitly match migration table
+      timestamps: true, // Sequelize will manage createdAt & updatedAt
+    }
+  );
+
+  return User;
+};
