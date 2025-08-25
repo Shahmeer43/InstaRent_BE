9b120ef1a46fb8ad6690c8e110391b8bd08f3c80
Shahmeer <shahmeerhameed59@gmail.com>
2025-08-26 03:18:00 +0500

    sign up controller work in progress

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,20 @@
-exports.signUp = async (req, res) => {};
+const { User } = require("../models");
+
+exports.signUp = async (req, res) => {
+  try {
+    const [user, created] = await User.findOrCreate({
+      where: { email: req.body.email },
+      defaults: {
+        username: req.body.username,
+        password: req.body.password,
+      },
+    });
+    if (created) {
+      res.status(201).json({ message: "User created", user });
+    } else {
+      res.status(200).json({ message: "User already exists", user });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
