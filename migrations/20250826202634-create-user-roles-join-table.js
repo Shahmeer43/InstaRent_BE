875f67cd1d7e984de1f587a79f37f1f2eeb94ff1
Shahmeer <shahmeerhameed59@gmail.com>
2025-08-27 02:22:59 +0500

    added join user-roles table

---
+++ migrations/20250826202634-create-user-roles-join-table.js
@@ -0,0 +1,55 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("user_roles", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users", // name of the target table
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      roleId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "roles", // name of the target table
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+    });
+
+    // add a composite unique constraint so a user can’t have the same role twice
+    await queryInterface.addConstraint("user_roles", {
+      fields: ["userId", "roleId"],
+      type: "unique",
+      name: "unique_user_role",
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("user_roles");
+  },
+};
