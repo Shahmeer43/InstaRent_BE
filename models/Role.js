1a863344bef4dcbd38df713079d61440e5c94454
Shahmeer <shahmeerhameed59@gmail.com>
2025-08-28 01:11:46 +0500

    added roles model

---
+++ models/Role.js
@@ -0,0 +1,29 @@
+"use strict";
+
+module.exports = (sequelize, DataTypes) => {
+  const Role = sequelize.define(
+    "Role",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      isPublic: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "roles",
+      timestamps: false,
+    }
+  );
+
+  return Role;
+};
