385a7a609bc06f4a4920aaeae169f69f5fe40993
Shahmeer <shahmeerhameed59@gmail.com>
2025-09-01 01:39:03 +0500

    added new constants for login

--- shared/constants/responseMessages.js
+++ shared/constants/responseMessages.js
@@ -1,6 +1,8 @@
 const responseMessages = {
   INVALID_ROLE: "Invalid role",
+  USER_NOT_FOUND: "User not found",
   USER_ALREADY_EXIST: "User already exist",
+  INVALID_CREDENTIALS: "Invalid credentials",
   USER_REGISTERED_SUCCESSFULLY: "User registered successfully",
 };
 
