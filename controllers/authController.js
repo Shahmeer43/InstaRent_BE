92558c5a3a0c881db6c25c69b23992936fcff97e
Shahmeer <shahmeerhameed59@gmail.com>
2025-09-01 01:55:05 +0500

    login controller completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,16 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const { User, Role } = require("../models");
 const { responseMessages } = require("../shared/constants");
 
-const { INVALID_ROLE, USER_ALREADY_EXIST, USER_REGISTERED_SUCCESSFULLY } =
-  responseMessages;
+const {
+  INVALID_ROLE,
+  USER_NOT_FOUND,
+  USER_ALREADY_EXIST,
+  INVALID_CREDENTIALS,
+  USER_REGISTERED_SUCCESSFULLY,
+} = responseMessages;
 
 exports.signUp = async (req, res) => {
   const { email, username, password, roleId } = req.body;
@@ -29,6 +35,43 @@ exports.signUp = async (req, res) => {
     await user.addRole(role);
 
     res.status(201).json({ message: USER_REGISTERED_SUCCESSFULLY });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.logIn = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ where: { email } });
+    if (!user) return res.status(404).json({ message: USER_NOT_FOUND });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(401).json({ message: INVALID_CREDENTIALS });
+
+    const accessToken = jwt.sign(
+      { id: user.id },
+      process.env.JWT_ACCESS_SECRET_KEY,
+      {
+        expiresIn: "5m",
+      }
+    );
+
+    const refreshToken = jwt.sign(
+      { id: user.id },
+      process.env.JWT_REFRESH_SECRET_KEY,
+      { expiresIn: "7d" }
+    );
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "Lax",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({ accessToken });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
