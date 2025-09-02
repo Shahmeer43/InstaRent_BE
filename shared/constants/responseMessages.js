3877412819425a86bd8e55fe61a1fca59acdd02a
Shahmeer <shahmeerhameed59@gmail.com>
2025-09-03 00:27:04 +0500

    added new response messages

--- shared/constants/responseMessages.js
+++ shared/constants/responseMessages.js
@@ -1,6 +1,8 @@
 const responseMessages = {
   INVALID_ROLE: "Invalid role",
+  INVALID_TOKEN: "Invalid token",
   USER_NOT_FOUND: "User not found",
+  TOKEN_NOT_FOUND: "Token not found",
   USER_ALREADY_EXIST: "User already exist",
   INVALID_CREDENTIALS: "Invalid credentials",
   USER_REGISTERED_SUCCESSFULLY: "User registered successfully",
